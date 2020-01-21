3bdc6fbe21275dd3dbbd9f3919d942a646db7aa6
Waynezhai <zhyzhaihuiyan@163.com>
2020-01-21 17:45:17 +0800

    修改文件名

---
+++ lxf_001_helloWorld.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+################################################################# 
+# FileName: lxf_helloWorld.py
+# Author: Wayne_zhy
+# Mail: zhyzhaihuiyan@163.com
+# Created Time: 2020-01-21 16:19:46
+# Last Modified: 2020-01-21 16:23:49
+################################################################# 
+
+print("hello world!")
+
+print(100 + 200 + 300)
+
+print(2**10)
